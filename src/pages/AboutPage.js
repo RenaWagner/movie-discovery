378e858001aeeddbe301582ded9a7d3c1b610bbc
Rena Suzuki Wagner <rena.suzuki.wagner@gmail.com>
2021-02-24 17:27:48 +0100

    Added a bit of description

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,6 +4,11 @@ export default function AboutPage() {
   return (
     <div>
       <p>AboutPage</p>
+      <p>
+        Welcome to my movie discovery page! This is my first React project that
+        I worked in the first week of Codaisseur! Click to Movie Discovery to
+        discover new movies!
+      </p>
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function AboutPage() {
   return (
     <div>
-      <p>HomePage</p>
+      <h2>HomePage</h2>
+      <p>
+        Welcome to my movie discovery page! This is my first React project that
+        I worked in the first week of Codaisseur! Click to Movie Discovery to
+        discover new movies!
+      </p>
     </div>
   );
 }
